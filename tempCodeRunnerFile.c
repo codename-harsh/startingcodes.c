57baef97e93ef47f03808aac918bf0b24aabe5d8
Harshvardhan Singh <hvsingh46@gmail.com>
2025-12-04 23:46:19 +0530

    codes i have practiced till 4/12/25

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,23 +1,24 @@
 #include<stdio.h>
 #include<math.h>
-int fact(int n)
-{
+int fac(int n){
     int f=1;
-    for(int i=1;i<=n;i++)
+    for(int i=1;i<=n;i++){
     f*=i;
+    }
     return f;
 }
-int ncr(int n, int r)
-{
-    return fact(n)/(fact(r)*fact(n-r));
+int ncr(int n, int r){
+    return fac(n)/(fac(r)*fac(n-r));
 }
 int main(){
     int n;
-    printf("Enter n: ");
+    printf("Enter the number of rows : ");
     scanf("%d",&n);
-    int r;
-    printf("Enter r: ");
-    scanf("%d",&r);
-    int ans = ncr(n,r);
-    printf("nCr = %d\n",ans);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            int x = ncr(i,j);
+            printf("%d ",x);
+        }
+        printf("\n");
+    }
 }
