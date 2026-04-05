ba6ff9297e39b59ad9420445201fd6bd4ad8b09f
Harshvardhan Singh <hvsingh46@gmail.com>
2026-04-06 01:10:02 +0530

    minor changes

--- PascalTriangle.c
+++ PascalTriangle.c
@@ -15,12 +15,11 @@ int main(){
     printf("Enter the number of rows : ");
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
-        for(int k=1;k<=n-i-1;k++){
+        for(int k=1;k<=n-i;k++){
             printf(" ");
         }
         for(int j=1;j<=i;j++){
-            int x = ncr(i,j);
-            printf("%d ",x);
+            printf("%d ",ncr(i-1,j-1));
         }
         printf("\n");
     }
--- Pattern_Printing/tempCodeRunnerFile.c
+++ Pattern_Printing/tempCodeRunnerFile.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main(){
-    int n;
-    puts("Enter the number of rows : ");
-    scanf("%d",&n);
+    int n=3;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++)
-            printf("*");
+        for(int s=1;s<=n-i;s++) printf(" ");
+        for(int j=1;j<=2*i-1;j++) printf("*");
+        printf("\n");
+    }
+    for(int i=n-1;i>=1;i--){
+        for(int s=1;s<=n-i;s++) printf(" ");
+        for(int j=1;j<=2*i-1;j++) printf("*");
         printf("\n");
     }
 }
